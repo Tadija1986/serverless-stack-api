7daf112463ef285b28cd5ef080394322b5bb2450
nikola.tadic <nikola.tadic@devinity.io>
2020-06-10 16:02:41 +0200

    Adding some faulty code

--- get.js
+++ get.js
@@ -1,3 +1,27 @@
+// import handler from "./libs/handler-lib";
+// import dynamoDb from "./libs/dynamodb-lib";
+
+// export const main = handler(async (event, context) => {
+//   const params = {
+//     TableName: process.env.tableName,
+//     // 'Key' defines the partition key and sort key of the item to be retrieved
+//     // - 'userId': Identity Pool identity id of the authenticated user
+//     // - 'noteId': path parameter
+//     Key: {
+//       userId: event.requestContext.identity.cognitoIdentityId,
+//       noteId: event.pathParameters.id
+//     }
+//   };
+
+//   const result = await dynamoDb.get(params);
+//   if ( ! result.Item) {
+//     throw new Error("Item not found.");
+//   }
+
+//   // Return the retrieved item
+//   return result.Item;
+// });
+
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
@@ -9,7 +33,7 @@ export const main = handler(async (event, context) => {
     // - 'noteId': path parameter
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: event.pathParameters.id
+      // noteId: event.pathParameters.id
     }
   };
 
@@ -17,7 +41,7 @@ export const main = handler(async (event, context) => {
   if ( ! result.Item) {
     throw new Error("Item not found.");
   }
-
+  
   // Return the retrieved item
   return result.Item;
 });
